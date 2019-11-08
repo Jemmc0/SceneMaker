c3d9b8eecb788e0ce03ea897f1f007b93f6507c9
Ignacio Fernandez <ignacio.fernandez1@davinci.edu.ar>
2019-11-08 03:46:42 -0300

    mapeo

---
+++ SceneMaker/Assets/Editor/SceneMaker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(Waypoints))]
+
+public class SceneMaker : Editor
+{
+    //private Custom _custom;
+    private Waypoints _waypoints;
+
+    private void OnEnable()
+    {
+        //_custom = (Custom)target;
+        _waypoints = (Waypoints)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawHeroParameters();
+    }
+
+    private void DrawHeroParameters()
+    {
+        _waypoints.countx = EditorGUILayout.IntField("GridX", _waypoints.countx);
+        _waypoints.countz = EditorGUILayout.IntField("GridY", _waypoints.countz);
+        if (GUILayout.Button("Grilla"))
+            Grid();
+    }
+
+    void Grid()
+    {
+        _waypoints.CreateTarget(_waypoints.countx,_waypoints.countz);
+    }   
+
+        
+}
---
+++ SceneMaker/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Waypoints : MonoBehaviour
+{
+    public int GridX;
+    public int GridY;
+    public float x, y, z;
+    public float sizeX, sizeY, sizeZ, posY;
+    public int countx, countz;
+    public GameObject[,] testArray;
+    public int i, j;
+    RaycastHit hit;
+
+    void Update()
+    {
+                
+    }
+    public void CreateTarget(int countX, int countY)
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+            DestroyImmediate(gameObject.transform.GetChild(i).gameObject,false);
+
+
+        sizeX = GetComponent<Renderer>().bounds.size.x;
+        sizeY= GetComponent<Renderer>().bounds.size.y;
+        sizeZ = GetComponent<Renderer>().bounds.size.z;
+        x = sizeX / countX;
+        z = sizeZ / countY;
+        
+        testArray =new GameObject[countX+1,countY+1];
+        for (i = 0; i < countX+1; i++)
+        {
+            testArray[i , j] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            testArray[i, j].transform.position = transform.position + new Vector3((-sizeX / 2) + (i * x), (sizeY / 2), (-sizeZ / 2) + (j * z));
+            testArray[i, j].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            testArray[i, j].transform.SetParent(gameObject.transform);
+            testArray[i, j].name = "Waypont "+ i + "," + j;
+            //testArray[i, j].GetComponent<MeshRenderer>().enabled = false;
+            
+
+            for (j = 1; j < countY+1; j++)
+            {
+                testArray[i, j] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                testArray[i, j].transform.position = transform.position + new Vector3((-sizeX / 2) + (i * x), (sizeY / 2), (-sizeZ / 2) + (j * z));
+                testArray[i, j].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                testArray[i, j].transform.SetParent(gameObject.transform);
+                testArray[i, j].name = "Waypont "+ i + "," + j;
+                //testArray[i, j].GetComponent<MeshRenderer>().enabled = false;
+
+
+
+            }
+            j =0 ;
+        }
+        
+
+        /*for (int i = 0; i <countx ; i++)
+        {
+            GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            for (int j = 0; j < countz; j++)
+            {
+                GameObject.CreatePrimitive(PrimitiveType.Cube);
+            }
+        }*/
+
+        /*
+        if (wp != null)
+            Destroy(wp);
+        GameObject waypoint = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        waypoint.transform.position = new Vector3(Random.Range(7.0f, -7.0f), 0.03f,
+        Random.Range(7.0f, -7.0f));
+        waypoint.name = "Waypoint " + id;
+        id++;
+        waypoint.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        //waypoint.GetComponent<MeshRenderer>().enabled = false;
+        wp = waypoint;*/
+    }
+}
